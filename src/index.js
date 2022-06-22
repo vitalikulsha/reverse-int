88b2c59a37e5612d039d4146b8b4c350f499f3f9
Vitali Kulsha <73823615+vitalikulsha@users.noreply.github.com>
2022-06-22 09:28:45 +0300

    feat: update function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
     let result = [];
     Array.from(String(num)).forEach(n => result.unshift(n));
-    return +result.join('');
+    return result.join('');
 }
