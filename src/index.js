39683b42ea2dffe962cc6c66da07a848b97a3098
Vitali Kulsha <73823615+vitalikulsha@users.noreply.github.com>
2022-06-22 09:57:26 +0300

    fix: update code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 module.exports = function reverse (n) {
     let result = [];
     Array.from(String(Math.abs(n))).forEach(num => result.unshift(num));
-    if(n < 0){
-      result.unshift('-');
-    }
     return Number(result.join(''));
 }
